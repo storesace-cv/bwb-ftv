ee22aabe26176d3955471e076219194e6c599cb5
storesace-cv <153992141+storesace-cv@users.noreply.github.com>
2025-09-20 15:20:11 +0100

    Clone B1.A1.A.A.2 zone into third slot

--- ui/ui_editor_fonte.py
+++ ui/ui_editor_fonte.py
@@ -741,8 +741,8 @@ class FTApp(QWidget):
         )
         self.B1A1.ly.addWidget(self.B1A1A, 0)
         self.B1A1AA, self.B1A1AB = self.B1A1A.split_h((3, 1))
-        self.B1A1AAA, self.B1A1AAB = self.B1A1AA.split_v((1, 1))
-        for zone in (self.B1A1AAA, self.B1A1AAB):
+        self.B1A1AAA, self.B1A1AAB, self.B1A1AAC = self.B1A1AA.split_v((1, 1, 1))
+        for zone in (self.B1A1AAA, self.B1A1AAB, self.B1A1AAC):
             zone.ly.setContentsMargins(0, 0, 0, 0)
             current_policy = zone.sizePolicy()
             zone.setSizePolicy(
