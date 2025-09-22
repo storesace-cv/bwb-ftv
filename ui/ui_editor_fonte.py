66a4f2b0bb36ae5e082cba4a416dd0c6dd662158
storesace-cv <153992141+storesace-cv@users.noreply.github.com>
2025-09-22 02:18:07 +0100

    Adjust family zone spacing and add regression test

--- ui/ui_editor_fonte.py
+++ ui/ui_editor_fonte.py
@@ -795,6 +795,8 @@ class FTApp(QWidget):
                 current_policy.verticalPolicy(),
             )
 
+        self.zone_general_aux_family_slot.ly.setContentsMargins(0, 8, 0, 0)
+
         self.zone_general_aux_identification_slot.ly.setContentsMargins(0, 8, 0, 8)
         self.zone_general_aux_identification_slot.apply_metadata(
             zone_type="coluna-campos",
